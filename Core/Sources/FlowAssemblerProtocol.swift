3f65ff9059b61de70ad6e632b07a7eae305273fb
Hinomori Hiroya <nexearth@gmail.com>
2020-07-08 18:17:36 +0900

    Add: FlowController classes

---
+++ Core/Sources/FlowAssemblerProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  FlowAssemblerProtocol.swift
+//  Core
+//
+//  Created by hinomori-hiroya on 2020/07/08.
+//
+
+import UIKit
+
+public protocol FlowAssemblerProtocol {
+    func root() -> FlowController
+    // TODO: Add directions
+}
